3ea6ced2d02f4c620b4fa2429a9be57acdd5f25e
 ahmedfarazshah <ahmedfarazshah42@gmail.com>
2023-08-07 18:52:22 +0500

    do while

---
+++ Labs (August)/do_while.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+contract Loop{
+    //The value of cond that you will input will decide 
+    //the number of times the loop is run
+    function iterate(uint cond) public pure returns(uint){
+        uint i;
+        uint iteration;
+        do {
+        iteration ++;
+        i++;
+        } while(i < cond);
+        //If you input the value of 'cond' as 0, the condition will be false
+        //on the first try, however you will notice that the loop still runs once
+        return iteration;
+    }
+}
+
+contract Looped{
+    uint[] public arr;
+    function ODDNum(uint max) public{
+        uint i = 1;
+        while(i <= max){
+        if (i % 2 != 0)
+        arr.push(i);
+        i++;
+        }
+        }
+        function get() public view returns(uint[] memory){
+        return arr;
+        }
+}
