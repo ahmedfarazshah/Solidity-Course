bb5c36cef0fe61394058c22e51a2041e416bf4e1
 ahmedfarazshah <ahmedfarazshah42@gmail.com>
2023-08-25 17:38:46 +0500

    try catch

--- Labs (August)/try_catch_library/new_composition.sol
+++ Labs (August)/try_catch_library/new_composition.sol
@@ -44,18 +44,27 @@ contract HMS{       //Hospital Management System
 
 contract patientlist{
     
-    HMS[] public list;   
+    HMS[] public patients;
+    
 
+    constructor(){
+        HMS newpatient = new HMS('Harry',89,'fever');
+        patients.push(newpatient);
 
-
+    }
     // we cant use function instead of the constructor because we want to add the value in the runtime
     // function newIn(string memory _name,uint _age, string memory _disease)public {
         
-
-
+}   
+/*  just to check with in the inheritance
+contract person is HMS('fine',33,'nil'),patientlist{
+    constructor(){}
+    string public show = "direct initialized but new values can't be saved";
+}
+contract GetPatient is HMS{
+    constructor(string memory _namez,uint tem_age,string memory _healthissue )HMS(_namez,tem_age,_healthissue){}
 
 }
-
-
+*/
 
 
