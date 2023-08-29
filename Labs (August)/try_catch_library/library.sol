115e544a88f445d6ebb53b556312214de7515980
ahmedfarazshah <ahmedfarazshah42@gmail.com>
2023-08-29 15:41:24 +0500

    try catch done

--- Labs (August)/try_catch_library/library.sol
+++ Labs (August)/try_catch_library/library.sol
@@ -3,3 +3,12 @@ pragma solidity ^0.8.17;
 
 // library <name> {}
 
+contract kufe{
+
+    event naming(string name);
+
+    function what (string memory person)public  {
+        emit naming(person);
+
+    }   
+}
